0540afedc10f2473993853fa4b961d451ca44663
Keith <keithtmccartney@hotmail.com>
2017-08-17 22:27:03 +0100

    Adding a BDD structure.

---
+++ PoloniexNet.Web/PoloniexNet.Web.Tests/Features/Calculator.feature
@@ -0,0 +1,51 @@
+﻿Feature: Calculator
+	I want to test the behavior of a Calculator
+
+Scenario: Add two numbers
+	Given I have a Calculator
+	When I add 10 to 25
+	Then the result should be 35
+
+@Calculator 
+Scenario Outline: Add two number
+	Given I have a Calculator
+	When I add <first> to <second>
+	Then the result should be <result>
+	
+	Examples: 
+	| first | second | result |
+	| 17    | 25     | 42     |
+	| 10    | 78     | 88     |
+	| 2     | 25     | 27     |
+	| 5     | -17    | -12    |
+
+@Calculator
+Scenario Outline: Subtract two numbers
+	Given I have a Calculator
+	When I subtract <first> to <second>
+	Then the result should be <result>
+	Examples: 
+	| description | first | second | result |
+	| 17_59       | 17    | 59     | -42    |
+	| 10_3        | 10    | 3      | 7      |
+
+@Calculator
+Scenario Outline: Multiply two numbers
+Given I have a Calculator
+	When I multiply <first> to <second>
+	Then the result should be <result>
+	Examples: 
+	| description | first | second | result |
+	| 17*59       | 17    | 59     | 1003   |
+	| 10*3        | 10    | 3      | 30     |
+
+@Calculator
+Scenario Outline: Divide two numbers
+	Given I have a Calculator
+	When I divide <first> to <second>
+	Then the result should be <result>
+	Examples: 
+	| description | first | second | result |
+	| 89/7        | 89    | 7      | 12     |
+	| 106/3       | 106   | 3      | 35     |
+	| 20/2        | 20    | 2      | 10     |      
---
+++ PoloniexNet.Web/PoloniexNet.Web.Tests/Steps/Calculator.StepDefinition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shouldly;
+using TechTalk.SpecFlow;
+using PoloniexNet.Web;
+
+namespace super_duper_octo_goggles.Tests.Steps
+{
+    [Binding]
+    public class Calculator
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        /*private Program _calculator;
+
+        [Given(@"I have a Calculator")]
+        public void GivenIHaveACalculator()
+        {
+            _calculator = new Program();
+        }
+
+        [When(@"I add (.*) to (.*)")]
+        public void WhenIAddTo(int first, int second)
+        {
+            var result = _calculator.Add(first, second);
+
+            ScenarioContext.Current.Add("result", result);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            var actualresult = ScenarioContext.Current.Get<int>("result");
+
+            actualresult.ShouldBe(result);
+        }
+
+        [When(@"I subtract (.*) to (.*)")]
+        public void WhenISubtractTo(int first, int second)
+        {
+            var result = _calculator.Subtract(first, second);
+
+            ScenarioContext.Current.Add("result", result);
+        }
+
+        [When(@"I multiply (.*) to (.*)")]
+        public void WhenIMultiplyTo(int first, int second)
+        {
+            var result = _calculator.Multi(first, second);
+
+            ScenarioContext.Current.Add("result", result);
+        }
+
+        [When(@"I divide (.*) to (.*)")]
+        public void WhenIDivideTo(int first, int second)
+        {
+            var result = _calculator.Divide(first, second);
+
+            ScenarioContext.Current.Add("result", result);
+        }*/
+    }
+}
